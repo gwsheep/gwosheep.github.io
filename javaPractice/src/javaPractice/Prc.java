9199f7c3e16b82d74782028c94fe34c0c1e940ce
gwsheep <jae-kwen@daum.net>
2023-06-23 13:18:10 +0900

    commit 연습용

---
+++ javaPractice/src/javaPractice/Prc.java
@@ -0,0 +1,14 @@
+package javaPractice;
+
+public class Prc {
+	public static void main(String[] args) {
+
+		int a = 1;
+		int b = 2;
+		int c = a+b;
+		System.out.println(c);
+		
+		String d = "abc";
+		System.out.println(d);
+	}
+}
---
+++ javaPractice/src/javaPractice/Prc2.java
@@ -0,0 +1,14 @@
+package javaPractice;
+
+public class Prc2 {
+	public static void main(String[] args) {
+		
+		String a = "abc";
+		String b = "cdf";
+		String c = a+b;
+		System.out.println(c);
+		
+		
+		
+	}
+}
